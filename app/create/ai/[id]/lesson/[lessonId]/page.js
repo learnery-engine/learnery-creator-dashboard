916618430e1ff3f3e1bdb3868d3f08a8b7c3e0e2
jaysilver-5 <udojoshua3@gmail.com>
2023-07-03 16:24:54 +0100

    Build: implement router and page

--- app/create/ai/[id]/lesson/[lessonId]/page.js
+++ app/create/ai/[id]/lesson/[lessonId]/page.js
@@ -2,15 +2,15 @@
 
 import React from "react";
 import Image from "next/image";
-import { Typography } from "@mui/material";
 
 export default function Home() {
+
   return (
     <main>
 
 
       <div>
-        <h1>/create/ai/[:id]/lesson/[id]</h1>
+        <h1>/create/ai/Sid/lesson/[id]</h1>
       </div>
 
 
