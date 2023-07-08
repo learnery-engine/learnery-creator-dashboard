cf86c138fbcd5724db45cc35c99cd4c73c8a7e58
Chrisgusna <100565709+Chrisgusna@users.noreply.github.com>
2023-07-08 19:51:34 +0100

    Add files via upload

---
+++ components/frame160/Frame160.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'styles.css';
+
+const Frame160 = () => {
+  return (
+    <div className="container">
+      <div className="second-con">
+        <div className="first-div">
+          <p>Your course has been published successfully!</p>
+        </div>
+
+        <div className="second-div">
+          <p>Share</p>
+          <p>View course</p>
+          <p>View in Dashboard</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frame160;
